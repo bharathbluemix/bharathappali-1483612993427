1198d10fa94bb619699d39a08c3c3c6cd613248e
bharath appali <bharathbluemix@gmail.com>
2017-01-06 00:15:12 -0600

    new commit 1

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -4,9 +4,6 @@
 //Calls SimpleServlet to get the "Hello World" message
 xhrGet("SimpleServlet", function(responseText){
 	// add to document
-	var mytitle = document.getElementById('message');
-	mytitle.innerHTML = responseText;
-
 }, function(err){
 	console.log(err);
 });
@@ -32,7 +29,7 @@ function xhrGet(url, callback, errback){
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState == 4){
 			if(xhr.status == 200){
-				callback(xhr.responseText);
+				callback("hello");
 			}else{
 				errback('service not available');
 			}
