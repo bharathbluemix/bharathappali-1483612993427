118c4fef4640b1e2bdaf2134e0b128e8e01945a6
bharath appali <bharathbluemix@gmail.com>
2017-01-06 00:22:22 -0600

    new commit 3

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -20,7 +20,6 @@ public class SimpleServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
         response.sendRedirect("/twitlogin.html");
     }
 
